2acbb553059d61796c55d7287ad1f76efdef30e9
Prabhu Subramanian <prabhu@appthreat.com>
2026-06-28 11:09:50 +0530

    Publish to gcr
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- engine/src/main/scala/io/appthreat/chennai/engine/Main.scala
+++ engine/src/main/scala/io/appthreat/chennai/engine/Main.scala
@@ -18,7 +18,7 @@ object Main:
     import builder.*
     OParser.sequence(
       programName("chennai-engine"),
-      head("chennai-engine", "0.2.0"),
+      head("chennai-engine", "0.3.0"),
       opt[Unit]("serve").action((_, c) => c.copy(serve = true)).text("run the NDJSON stdio server"),
       opt[String]("atom").action((p, c) => c.copy(atom = Some(p))).text(
         "path to an existing .atom file to open"
